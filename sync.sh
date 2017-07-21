5ea65e5ea73f3d73e0520457e853c4280f5da18d
Tong Niu <tn66@duke.edu>
2017-07-21 14:37:22 -0400

    first page

---
+++ sync.sh
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Hello Coursera! Haha!</title>
+</head>
+<body>
+<h1>Hello Coursera! Haha!</h1>
+<p>I meant exactly that.</p>
+</body>
+</html> 
