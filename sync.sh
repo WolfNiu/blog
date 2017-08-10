a84e670be2f1d5e6aee8d0987f22091c2a03012c
Tong Niu <tn66@duke.edu>
2017-08-10 15:01:41 -0400

    updated sync.sh

--- sync.sh
+++ sync.sh
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Hello Coursera! Haha!</title>
-</head>
-<body>
-<h1>Hello Coursera! Haha!</h1>
-<p>I meant exactly that.</p>
-</body>
-</html> 
+browser-sync start --server --dicrectory --files "*"
